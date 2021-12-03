d75b01c1f364095ebf13b297bafd4a697d29e988
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2021-12-03 21:48:04 +0100

    Cleaned up puzzle 2

--- src/day3/puzzle2.py
+++ src/day3/puzzle2.py
@@ -4,41 +4,24 @@ sys.path.append(os.path.abspath("."))
 from utils import read_input
 import numpy as np
 
-def oxygen_rating_step(position, index):
+def step(position, index, rating):
     valid_numbers = position[index]
-    # Keep ones if ones are the most common
-    keep_ones = np.sum(valid_numbers) >= len(valid_numbers) / 2
+    if rating == "oxygen":
+        # Keep ones if ones are the most common
+        keep_ones = np.sum(valid_numbers) >= len(valid_numbers) / 2
+    elif rating == "co2_scrubber":
+        # Keep ones if ones are the least common
+        keep_ones = np.sum(valid_numbers) < len(valid_numbers) / 2
     ones_index = position == 1
     if keep_ones:
         return index * ones_index
     else:
         return index * ~ones_index
 
-def c02_scrubber_rating_step(position, index):
-    valid_numbers = position[index]
-    # Keep ones if ones are the least common
-    keep_ones = np.sum(valid_numbers) < len(valid_numbers) / 2
-    ones_index = position == 1
-    if keep_ones:
-        return index * ones_index
-    else:
-        return index * ~ones_index
-
-def find_oxygen_rating(bit_array):
+def find_rating(bit_array, rating):
     index = np.ones((bit_array.shape[0],), dtype=bool)
     for i in range(bit_array.shape[1]):
-        index = oxygen_rating_step(bit_array[:, i], index)
-        if np.sum(index) == 1:
-            return int(
-                "".join((str(x) for x in np.squeeze(bit_array[index, :]))),
-                base=2
-            )
-    return None
-
-def find_c02_scrubber_rating(bit_array):
-    index = np.ones((bit_array.shape[0],), dtype=bool)
-    for i in range(bit_array.shape[1]):
-        index = c02_scrubber_rating_step(bit_array[:, i], index)
+        index = step(bit_array[:, i], index, rating=rating)
         if np.sum(index) == 1:
             return int(
                 "".join((str(x) for x in np.squeeze(bit_array[index, :]))),
@@ -51,6 +34,6 @@ input_data = read_input(INPUT_PATH)
 
 bit_array = np.stack([np.array(list(x), dtype=int) for x in input_data])
 
-output = find_oxygen_rating(bit_array) * find_c02_scrubber_rating(bit_array)
+output = find_rating(bit_array, "oxygen") * find_rating(bit_array, "co2_scrubber")
 
 print(output) 
